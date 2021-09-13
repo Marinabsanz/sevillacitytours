272ed14cd07daa84c846b6596da60e22b338848a
Marina <marinabsanz@gmail.com>
2021-09-13 18:59:43 +0200

    addtocarrito

--- js/app.js
+++ js/app.js
@@ -12,8 +12,8 @@ const vaciarCarritoBtn  = document.querySelector ('#vaciar-carrito')
 
 cargarEventListeners();
 function cargarEventListeners ( ) {
-    //cuando agregas un curso seleccioonando 'agregar carrito'
-    listaCursos.addEventListener('click', agregarTour);
+    //cuando agregas un tour seleccioonando 'reserva tu tour'
+    listaTours.addEventListener('click', agregarTour);
 
 }
 
@@ -24,6 +24,25 @@ function cargarEventListeners ( ) {
 function agregarTour(e) {
     e.preventDefault()
     if(e.target.classList.contains('agregar-carrito')) { 
-    console.log (e.target)
+    const tourSelected = e.target.parentElement.parentElement;
+
+    addToCarrito (tourSelected);
 }
+}
+
+//leer contenido del html al dar click para meter info en carrito
+
+function addToCarrito (tourSelected) {
+    console.log(tourSelected);
+
+const infoTour = {
+    imagen:tourSelected.querySelector('img').src,
+    titulo: tourSelected.querySelector('h4').textContent,
+    precio: tourSelected.querySelector('.precio span').textContent,
+    
+
+   
+}
+
+console.log(infoTour)
 }
